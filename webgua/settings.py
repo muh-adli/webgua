92c24a7ea2a59114fdb576ba31b04f0998dbe406
Muhammad Adli <muhammadadli.eng@gmail.com>
2024-07-24 01:32:32 +0700

    set-up setttings for vercel app

--- webgua/settings.py
+++ webgua/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-9v_dbw41%u$a_$r$m@1=i&s02zticl-uk31h9pq%mekoa38t83
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
